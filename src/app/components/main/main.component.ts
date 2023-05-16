2fd136d571f77e5464f4fcf705c26ffe62de6e66
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2023-05-16 21:05:56 +0530

    final commit

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { weatherService } from 'src/app/services/weather.service';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { pluck, map } from 'rxjs';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-main',
@@ -15,27 +17,71 @@ export class main {
   faSpinner = faSpinner;
   spin: boolean = true;
   error: boolean = false;
+  Temps: number[] = [];
+  dates: number[] = [];
 
   constructor(private service: weatherService) {
-    
-      navigator.geolocation.getCurrentPosition(position => {
-        this.service.getLocation(position.coords.latitude, position.coords.longitude).subscribe(data => {
-          this.Temp = String(data.main.temp).split(".")[0];
-          this.City = data.name;
-          this.spin = false;
-          this.error = false;
-        }, err => {
-          this.error = true;
-          this.spin = false;
-        })
-      });
+
+    navigator.geolocation.watchPosition(function (position) {
+      // console.log("tracking you!");
+
+    }, error => {
+      if (error.code == error.PERMISSION_DENIED) {
+        this.handleWeather('visakhapatnam');
+      }
+    })
+
+    navigator.geolocation.getCurrentPosition(position => {
+      this.service.getLocation(position.coords.latitude, position.coords.longitude).subscribe(data => {
+        this.Temp = String(data.main.temp).split(".")[0];
+        this.City = data.name;
+        this.spin = false;
+        this.error = false;
+      }, err => {
+        this.error = true;
+        this.spin = false;
+      })
+
+      this.service.get7daysWeather_LatLon(position.coords.latitude, position.coords.longitude).pipe(
+        pluck('data'),
+        pluck('weather'),
+      ).subscribe(data => {
+
+        for (let i = 0; i < data.length; i++) {
+          this.Temps.push(parseInt(data[i].maxtempC));
+          this.dates.push(data[i].date);
+        }
+        // this.weatherForm.reset();
+        this.displayGraph();
+        this.Temps = []
+        this.dates = []
+      })
+
+    });
 
   }
 
-  handleWeather() {
+
+  displayGraph() {
+    var myChart = new Chart("myChart", {
+      type: 'line',
+      data: {
+        datasets: [{
+          label: 'Temperature',
+          data: this.Temps,
+          backgroundColor: "rgb(115 185 243 / 65%)",
+          borderColor: "#007ee7",
+          fill: true,
+        }],
+        labels: this.dates
+      },
+    });
+  }
+
+  handleWeather(city: String) {
 
     this.spin = true;
-    this.service.getWeather(this.weatherForm.value.city!).subscribe(data => {
+    this.service.getWeather(city).subscribe(data => {
       this.Temp = String(data.main.temp).split(".")[0];
       this.City = data.name;
       this.spin = false;
@@ -43,12 +89,31 @@ export class main {
     }, err => {
       this.error = true;
       this.spin = false;
-    }
-    )
+    })
+
+    this.service.get7daysWeather(city).pipe(
+      pluck('data'),
+      pluck('weather'),
+    ).subscribe(data => {
+
+      for (let i = 0; i < data.length; i++) {
+        this.Temps.push(parseInt(data[i].maxtempC));
+        this.dates.push(data[i].date);
+      }
+      // this.weatherForm.reset();
+      if (!this.error) {
+        this.displayGraph();
+      }
+      this.Temps = []
+      this.dates = []
+
+    })
+
   }
 
+
   weatherForm = new FormGroup({
-    city: new FormControl("", [Validators.required, Validators.pattern('^[a-zA-Z \-\']+')])
+    city: new FormControl("", [Validators.required, Validators.pattern('[a-zA-Z]*')])
   })
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -10,13 +10,25 @@ export class weatherService {
   constructor(private http:HttpClient) { }
 
   getLocation(lat: number, long:number): Observable<any> {
+    
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=387729f8d6de06a1c05a3ac870ffb50c`
     return this.http.get(url);
+
   } 
 
   getWeather(city: String): Observable<any>{
     
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=387729f8d6de06a1c05a3ac870ffb50c`
+    return this.http.get(url);
+  }
+
+  get7daysWeather(city: String): Observable<any> {
+    const url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=9d236d2b15b544bda1662827231505&q=${city}&num_of_days=7&format=json`;
+    return this.http.get(url);
+  }
+
+  get7daysWeather_LatLon(lat: number, long: number): Observable<any> {
+    const url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=9d236d2b15b544bda1662827231505&q=${lat},${long}&num_of_days=7&format=json`;
     return this.http.get(url);
   }
 }
